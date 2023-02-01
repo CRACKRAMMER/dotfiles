0bcd2b8026d5fa2b479856fcb367da07554a279d
CRACKRAMMER <2789475717@qq.com>
2023-02-02 00:35:32 +0800

    first commit

---
+++ scripts/.local/bin/linkWallpaper.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+dirpath=(~/Pictures/Wallpaper/Images/)
+if test $# -ne 1
+then
+    dirpath=$1
+    shift
+fi
+
+cd $dirpath && find . -type l -delete || exit 1
+
+if test -n "$1"
+then
+    [[ -e "$1" ]] && ln -sf $1/* . || exit 1
+else
+    find . -type f | xargs -I{} ln -sf {} . 
+fi
--- scripts/.local/bin/randomWallpaper.sh
+++ scripts/.local/bin/randomWallpaper.sh
@@ -1,19 +1,25 @@
 #/bin/bash
 
-kill `ps aux | grep -v -e grep -e $$ | grep randomWallpaper.sh | awk '{print $2}' | xargs echo` 2> /dev/null
-
-swww init 2> /dev/null
-
-swww img "`ls -d ~/Pictures/Wallpaper/Images/* | sort -R | head -n1`"
+# kill `ps aux | grep -v -e grep -e $$ | grep randomWallpaper.sh | awk '{print $2}' | xargs echo` 2> /dev/null
 kill `ps aux | grep -v -e grep -e $$ | grep -e randomVideoWallpaper.sh -e mpvpaper | awk '{print $2}' | xargs echo` 2> /dev/null
 
-while true;
-do
-    sleep 15m
-    swww img "`ls -d ~/Pictures/Wallpaper/Images/* | sort -R | head -n1`"
-  # ls -d ~/Pictures/Wallpaper/Images/* | sort -R | while read file;
-  # do
-  #   swww img "$file"
-  #   sleep 15m
-  # done
-done
+# process=`sh -c "ps aux | grep -v -e grep -e $$ | grep $(basename $0)"`
+# process=`echo $process | awk '{print $2}'`
+process=$(bash -c "ps -eo pid,comm,cmd | grep -v -e grep -e ps | grep $(basename $0)")
+process=`awk '$2 ~ "^(sh|bash)$" && $1 != '$$' {print $1}' <<< $process | sed 'N;s/\n/,/g'`
+echo $process
+
+if test -n "$process"
+then
+    kill `ps --ppid $process -o pid,ppid,comm | grep sleep | awk '{print $1}'`
+else
+    swww init
+    while true;
+    do
+        find -L ~/Pictures/Wallpaper/Images/ -maxdepth 1 -type f | sort -R | while read file;
+        do
+            swww img "$file"
+            sleep 10m
+        done
+    done
+fi
