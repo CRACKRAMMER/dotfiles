a8343a1ca5f7c0d293d2cad6cb0cb7485fe04a2f
CRACKRAMMER <2789475717@qq.com>
2023-02-03 03:31:21 +0800

    first commit

--- scripts/.local/bin/backlight-down.sh
+++ scripts/.local/bin/backlight-down.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-light=$(xbacklight -get | sed 's/\.[0-9]*//g')
+#light=$(xbacklight -get | sed 's/\.[0-9]*//g')
+light=$(light | cut -d'.' -f1)
 if test $[$light-10] -le 10
 then
     xbacklight -set 10
--- scripts/.local/bin/backlight-up.sh
+++ scripts/.local/bin/backlight-up.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-light=$(xbacklight -get | sed 's/\.[0-9]*//g')
+#light=$(xbacklight -get | sed 's/\.[0-9]*//g')
+light=$(light | cut -d'.' -f1)
 if test $[$light+10] -ge 100
 then
     xbacklight -set 100
