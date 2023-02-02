d52697a54ba62edb63d60b95c85ccfe861e690fd
CRACKRAMMER <2789475717@qq.com>
2023-02-03 04:18:33 +0800

    first commit

--- scripts/.local/bin/snapshot.sh
+++ scripts/.local/bin/snapshot.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
-grim -g "$(slurp)" - | swappy -f -
+case "$XDG_SESSION_TYPE" in
+    "wayland")
+        grim -g "$(slurp)" - | swappy -f -
+        ;;
+    "x11")
+        maim -g $(slop -l -c 0.3,0.4,0.6,0.4) | swappy -f -
+        ;;
+    *)
+        exit 1
+esac
