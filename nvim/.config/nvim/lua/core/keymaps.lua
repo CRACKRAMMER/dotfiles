6a158c90fc7a1c89629dacd4170b7fc3f4110092
CRACKRAMMER <2789475717@qq.com>
2023-07-27 12:07:02 +0800

    update lazy nvim

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -2,26 +2,38 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
--- ---------- 插入模式 ---------- ---
-keymap.set("i", "jk", "<ESC>")
+keymap.set("n","<leader>q","<Cmd>q<CR>");
+keymap.set("n","<leader>Q","<Cmd>qall<CR>");
+keymap.set("n","<leader>w","<Cmd>w<CR>");
+keymap.set("n","<leader>W","<Cmd>wall<CR>");
+keymap.set("n","<leader>x","<Cmd>x<CR>");
+keymap.set("n","<leader>X","<Cmd>xall<CR>");
+keymap.set("n","T","<cmd>tabnew<CR>")
 
--- ---------- 视觉模式 ---------- ---
--- 单行或多行移动
+keymap.set("n",";","<C-w>w")
+keymap.set("n","<C-a>","gg<S-v>G");
+
+keymap.set("n","<left>","<C-w><")
+keymap.set("n","<right>","<C-w>>")
+keymap.set("n","<up>","<C-w>+")
+keymap.set("n","<down>","<C-w>-")
+
+keymap.set("t","<ESC>","<C-\\><C-n>");
+keymap.set("n","<leader>tt",":vsplit<Return><C-w>w:terminal<Return>",{silent = true});
+
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- ---------- 正常模式 ---------- ---
--- 窗口
-keymap.set("n", "<leader>sv", "<C-w>v") -- 水平新增窗口 
-keymap.set("n", "<leader>sh", "<C-w>s") -- 垂直新增窗口
+keymap.set("n", "<leader>sv", "<C-w>v")
+keymap.set("n", "<leader>sh", "<C-w>s")
+keymap.set("n","<leader>sh","<C-w>h")
+keymap.set("n","<leader>sj","<C-w>j")
+keymap.set("n","<leader>sk","<C-w>k")
+keymap.set("n","<leader>sl","<C-w>l")
 
--- 取消高亮
 keymap.set("n", "<leader>nh", ":nohl<CR>")
 
--- 切换buffer
 keymap.set("n", "<C-L>", ":bnext<CR>")
 keymap.set("n", "<C-H>", ":bprevious<CR>")
 
--- ---------- 插件 ---------- ---
--- nvim-tree
 keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
