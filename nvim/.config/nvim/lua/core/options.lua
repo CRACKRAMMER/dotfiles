f5e77de8e4f63089f0b41dfa9e80fa4be873bcd0
CRACKRAMMER <2789475717@qq.com>
2023-07-27 12:12:27 +0800

    update lazy nvim

--- nvim/.config/nvim/lua/core/options.lua
+++ nvim/.config/nvim/lua/core/options.lua
@@ -1,36 +1,41 @@
 local opt = vim.opt
 
--- 行号
 opt.relativenumber = true
 opt.number = true
 
--- 缩进
+vim.scriptencoding = 'utf-8'
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
+opt.fileencodings = 'utf-8,gbk'
+
 opt.tabstop = 4
+opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 opt.autoindent = true
 
--- 防止包裹
 opt.wrap = false
 
--- 光标行
+opt.autoread = true
+vim.bo.autoread = true
+
+opt.scrolloff = 5
+opt.sidescrolloff = 5
+
+opt.hlsearch = true
+opt.incsearch = true
 opt.cursorline = true
 
--- 启用鼠标
 opt.mouse:append("a")
 
--- 系统剪贴板
 opt.clipboard:append("unnamedplus")
 
--- 默认新窗口右和下
 opt.splitright = true
 opt.splitbelow = true
 
--- 搜索
 opt.ignorecase = true
 opt.smartcase = true
 
--- 外观
 opt.termguicolors = true
 opt.signcolumn = "yes"
 vim.cmd[[colorscheme tokyonight-moon]]
