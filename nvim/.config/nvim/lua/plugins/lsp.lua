10f1d08e2978c47ce7f318ee6b3359ee4a69a77c
CRACKRAMMER <2789475717@qq.com>
2023-10-10 13:16:50 +0800

    update nvim config

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -16,14 +16,14 @@ require("mason-lspconfig").setup({
   },
 })
 
-require("mason-lspconfig").setup_handlers {
-    function (server_name) -- default handler (optional)
-        require("lspconfig")[server_name].setup {}
-    end,
-    ["rust_analyzer"] = function ()
-        require("rust-tools").setup {}
-    end
-}
+-- require("mason-lspconfig").setup_handlers {
+--     function (server_name) -- default handler (optional)
+--         require("lspconfig")[server_name].setup {}
+--     end,
+--     ["rust_analyzer"] = function ()
+--         require("rust-tools").setup {}
+--     end
+-- }
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
